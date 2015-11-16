9961c397679e6b886b60574309d7383a8eb3b368
darionyaphet <darion.yaphet@gmail.com>
2015-11-16 22:18:20 +0800

    push

---
+++ src/org/darion/yaphet/thread/CallBack.java
@@ -0,0 +1,12 @@
+package org.darion.yaphet.thread;
+
+import java.util.concurrent.Callable;
+
+public class CallBack implements Callable<String> {
+
+	@Override
+	public String call() throws Exception {
+		return null;
+	}
+
+}
