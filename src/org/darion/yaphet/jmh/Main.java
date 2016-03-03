b67fb056f2c3fcd2065ac6822e27fbe59d943949
darionyaphet <darion.yaphet@gmail.com>
2016-03-03 21:53:08 +0800

    daily publish

---
+++ src/org/darion/yaphet/jmh/Main.java
@@ -0,0 +1,18 @@
+package org.darion.yaphet.jmh;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+
+public class Main {
+
+	public void test() {
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder().include(".*" + Main.class.getSimpleName() + ".*").forks(1).build();
+		new Runner(opt).run();
+	}
+}
